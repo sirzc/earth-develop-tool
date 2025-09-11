e6766377dde164a1f56aff761679eeb877704118
zhouchao <zhouchao151724@163.com>
2025-09-11 10:07:31 +0800

    feature:引入hutool-core包进行功能维护

--- src/main/java/com/myth/earth/develop/ui/toolkit/views/TimestampToolViewImpl.java
+++ src/main/java/com/myth/earth/develop/ui/toolkit/views/TimestampToolViewImpl.java
@@ -15,16 +15,29 @@
 
 package com.myth.earth.develop.ui.toolkit.views;
 
+import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.DateUtil;
 import com.intellij.icons.AllIcons;
+import com.intellij.openapi.ui.ComboBox;
+import com.intellij.ui.GotItTooltip;
+import com.intellij.ui.JBColor;
 import com.intellij.ui.components.JBLabel;
+import com.intellij.ui.components.JBOptionButton;
+import com.intellij.ui.components.JBTextField;
 import com.intellij.ui.components.fields.ExtendableTextComponent;
 import com.intellij.ui.components.fields.ExtendableTextField;
+import com.intellij.util.ui.FormBuilder;
+import com.intellij.util.ui.JBUI;
 import com.myth.earth.develop.kit.ClipboardKit;
 import com.myth.earth.develop.ui.toolkit.core.Tool;
 import com.myth.earth.develop.ui.toolkit.core.ToolCategory;
+import org.apache.commons.lang.time.DateUtils;
+import org.jdesktop.swingx.HorizontalLayout;
+import org.jdesktop.swingx.VerticalLayout;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Date;
 
 /**
  * 示例
@@ -38,18 +51,56 @@ public class TimestampToolViewImpl extends AbstractToolView {
     private final ExtendableTextField extendableTextField;
 
     public TimestampToolViewImpl() {
+        Date currentDate = new Date();
+        String currentTimeMillis = String.valueOf(currentDate.getTime());
         // 带图标的按钮
         extendableTextField = new ExtendableTextField();
+        extendableTextField.setPreferredSize(JBUI.size(120, 35));
         extendableTextField.addExtension(ExtendableTextComponent.Extension.create(AllIcons.General.InlineCopyHover, AllIcons.General.InlineCopy, "复制", () -> {
             ClipboardKit.copy(extendableTextField.getText());
         }));
 
-        JPanel topPanel = new JPanel(new BorderLayout());
-        topPanel.add(new JBLabel("当前时间戳："), BorderLayout.WEST);
-        topPanel.add(extendableTextField, BorderLayout.CENTER);
-        // topPanel.add(Box.createHorizontalStrut(200), BorderLayout.EAST);
+        ComboBox<String> timeUnitBox = new ComboBox<>();
+        timeUnitBox.setPreferredSize(JBUI.size(80,35));
+        timeUnitBox.addItem("毫秒（ms）");
+        timeUnitBox.addItem("秒（s）");
 
-        add(topPanel, BorderLayout.NORTH);
+        ComboBox<String> timeZoneBox = new ComboBox<>();
+        timeZoneBox.setPreferredSize(JBUI.size(120,35));
+        timeZoneBox.addItem("Asia/Shanghai");
+        timeZoneBox.addItem("UTC");
+        timeZoneBox.addItem("GMT");
+        timeZoneBox.addItem("Asia/Tokyo");
+        timeZoneBox.addItem("America/New_York");
+        timeZoneBox.addItem("Europe/London");
+
+        JBTextField fromTimeField = new JBTextField(currentTimeMillis);
+        fromTimeField.setPreferredSize(JBUI.size(150, 35));
+
+        JBTextField toDateField = new JBTextField(DateUtil.format(currentDate, DatePattern.NORM_DATETIME_PATTERN));
+        toDateField.setPreferredSize(JBUI.size(150, 35));
+
+        JButton toDateButton = new JButton("转换");
+        toDateButton.setPreferredSize(JBUI.size(50, 35));
+
+        JPanel toDatePanel = new JPanel(new HorizontalLayout());
+        toDatePanel.add(fromTimeField);
+        toDatePanel.add(timeUnitBox);
+        toDatePanel.add(toDateButton);
+        toDatePanel.add(toDateField);
+        toDatePanel.add(timeZoneBox);
+
+        @SuppressWarnings("all")
+        JPanel centerPanel = FormBuilder.createFormBuilder()
+                                        .addComponent(new JBLabel("当前时间戳"))
+                                        .addComponent(extendableTextField)
+                                        .addComponent(new JBLabel("时间戳转日期时间"), 20)
+                                        .addComponent(toDatePanel)
+                                        .addComponent(new JBLabel("日期时间转时间戳"),20)
+                                        .addComponent(new JPanel())
+                                        .getPanel();
+
+        add(centerPanel, BorderLayout.NORTH);
     }
 
     @Override
